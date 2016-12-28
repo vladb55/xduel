e02a7e7ff6d5ffda9145c9dc0cb71fb0601bb4ec
Vlad <plzdiebyethx@gmail.com>
2016-12-29 01:45:31 +0600

    change port

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var express = require('express');
+﻿var express = require('express');
 
 var crypto = require('crypto');
 
@@ -138,6 +138,6 @@ app.post('/api/users/win', function(request, response) {
     });
 });
 
-app.listen(3333, function() {
+app.listen(3000, function() {
     console.log('App was launched');
 });
